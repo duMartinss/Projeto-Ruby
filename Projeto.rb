79155a485f79199bf543a06f0e83000ad9cb8378
Eduardo Martins <144805284+duMartinss@users.noreply.github.com>
2024-06-10 19:02:02 -0300

    Add files via upload

---
+++ Projeto.rb
@@ -0,0 +1,13 @@
+print "Digite seu nome: "
+
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+
+idade = gets.chomp.to_i
+
+puts "Seu usuario se chama #{nome} #{sobrenome}, e tem #{idade} anos."
